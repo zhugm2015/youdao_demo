1f0a63ab1ae2cf6c636d2e2ab023d4661d63e332
zhugm2015 <286161817@qq.com>
2015-12-19 16:24:18 +0800

    spring

--- src/main/java/ioc/c/Test.java
+++ src/main/java/ioc/c/Test.java
@@ -1,11 +1,18 @@
 package ioc.c;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by Administrator on 2015/12/19.
  */
 public class Test {
     public static void main(String[] args) {
-        Business business=new Business(new USBWriter());
+       /* Business business=new Business(new USBWriter());
+        business.write();*/
+
+        ApplicationContext applicationContext =new ClassPathXmlApplicationContext("applicationContext.xml");
+        Business business= (Business) applicationContext.getBean("business");
         business.write();
     }
 }

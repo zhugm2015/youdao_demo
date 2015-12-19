11a8edab7ea379338061adde25873634e95c5e86
zhugm2015 <286161817@qq.com>
2015-12-19 16:51:27 +0800

    spring

--- src/main/java/ioc/c/Business.java
+++ src/main/java/ioc/c/Business.java
@@ -6,7 +6,11 @@ package ioc.c;
 public class Business {
     private DeviceWriter deviceWriter;
 
-    public Business(DeviceWriter deviceWriter) {
+   /* public Business(DeviceWriter deviceWriter) {
+        this.deviceWriter = deviceWriter;
+    }*/
+
+    public void setDeviceWriter(DeviceWriter deviceWriter) {
         this.deviceWriter = deviceWriter;
     }
 
